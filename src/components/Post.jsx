5b263f4cdfa50881666472c850084ea2e3f8f015
Matheus Sanchez <theus.sanchez@gmail.com>
2023-10-17 22:27:24 -0300

    Correção do erro de key do react

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -48,9 +48,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(item => {
                     if (item.type === 'paragraph') {
-                        return <p>{item.content}</p>
+                        return <p key={item.content}>{item.content}</p>
                     } else if (item.type === 'link') {
-                        return <p><a href="#">{item.content}</a></p>
+                        return <p key={item.content}><a href="#">{item.content}</a></p>
                     }
                 })}
 
@@ -75,7 +75,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
 
             </div>
